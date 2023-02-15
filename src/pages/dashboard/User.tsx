06e7a4c6ceeef603774f2c4889efa9221a75c255
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-02-15 23:29:43 +0100

    feat: Implement basic structure of user details page

--- src/pages/dashboard/User.tsx
+++ src/pages/dashboard/User.tsx
@@ -1,6 +1,31 @@
 import { useParams } from "react-router-dom";
+import { useAppContext } from "../../context/context";
 
 export default function User() {
   const { userId } = useParams();
-  return <div>User {userId}</div>;
+  const { userList } = useAppContext();
+  const user = userList[Number(userId)];
+  return (
+    <div>
+      <h1>User details</h1>
+      <button>Blacklist user</button>
+      <button>Activate user</button>
+      <section>
+        <h1>Personal information</h1>
+        {JSON.stringify(user.profile)}
+      </section>
+      <section>
+        <h1>Education and employment</h1>
+        {JSON.stringify(user.education)}
+      </section>
+      <section>
+        <h1>Socials</h1>
+        {JSON.stringify(user.socials)}
+      </section>
+      <section>
+        <h1>Guarantor</h1>
+        {JSON.stringify(user.guarantor)}
+      </section>
+    </div>
+  );
 }
