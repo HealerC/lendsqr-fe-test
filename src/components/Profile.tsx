ba81b9ec25c9c84676e709b1a7f6e50fb10a231e
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-02-21 00:48:02 +0100

    feat: Create the user profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,57 @@
+import Avatar from "@mui/material/Avatar";
+// import profilePicture from "../assets/images/profile-picture.jpg";
+import arrowDownIcon from "../assets/icons/arrow-down.svg";
+import "./Profile.scss";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+
+type MenuProp = {
+  text: string;
+  action: () => void;
+};
+type Props = {
+  username: string;
+  profilePicturePath: string;
+  menuProps: MenuProp[];
+};
+export default function Profile({
+  username,
+  profilePicturePath,
+  menuProps,
+}: Props) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleOpen = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button className="profile" onClick={handleOpen}>
+        <Avatar alt={username} src={profilePicturePath} />
+        <p>{username}</p>
+        <img src={arrowDownIcon} />
+      </Button>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        {menuProps.map((item, index) => (
+          <MenuItem
+            key={index}
+            onClick={() => {
+              item.action();
+              handleClose();
+            }}
+          >
+            {item.text}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+}
