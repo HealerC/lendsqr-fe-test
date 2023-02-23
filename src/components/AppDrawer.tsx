9542e9aacd5afa46e32f21d10f65ca8bd24013f4
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-02-23 12:51:30 +0100

    feat: Show the app bar on top of the drawer

--- src/components/AppDrawer.tsx
+++ src/components/AppDrawer.tsx
@@ -1,4 +1,5 @@
 import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
 import DrawerItemsComponent from "./DrawerItemsComponent";
 import { useAppContext } from "../context/context";
 import "./AppDrawer.scss";
@@ -14,9 +15,11 @@ export default function AppDrawer() {
         ModalProps={{ keepMounted: true }}
         className="temporary-drawer"
       >
+        <Toolbar />
         <DrawerItemsComponent />
       </Drawer>
       <Drawer variant="permanent" className="permanent-drawer" open>
+        <Toolbar />
         <DrawerItemsComponent />
       </Drawer>
     </nav>
