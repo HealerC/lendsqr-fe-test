a51677acf470846942a7b90d2d884bf183b08241
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-02-24 02:01:39 +0100

    feat: Create the summary card component

---
+++ src/components/CardSimple.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SummaryData } from "../utils/summary-content";
+import Card from "@mui/material/Card";
+import "./CardSimple.scss";
+
+export default function CardSimple({ icon, title, data }: SummaryData) {
+  return (
+    <Card variant="outlined" className="summary-card">
+      {icon}
+      <p>{title}</p>
+      <h3>{data}</h3>
+    </Card>
+  );
+}
