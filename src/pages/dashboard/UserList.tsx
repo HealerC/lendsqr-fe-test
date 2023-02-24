c2b7aaf3d1c8bc3a03e163456f7876ed4acb9797
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-02-24 02:02:16 +0100

    feat: Add the summary cards to the user list page

--- src/pages/dashboard/UserList.tsx
+++ src/pages/dashboard/UserList.tsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import { useAppContext } from "../../context/context";
 import { UserDetails, UserDetailsAPI, Status } from "../../context/interfaces";
 import { useNavigate } from "react-router-dom";
+import CardSimple from "../../components/CardSimple";
+import { summaryContent } from "../../utils/summary-content";
+import { UserDetailsSummary } from "../../context/interfaces";
+import "./UserList.scss";
 
 const apiUrl =
   "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users";
@@ -23,15 +27,21 @@ export default function UserList() {
     getUsers();
   }, []);
 
+  function getCards() {
+    return Object.keys(userListSummary).map((key) => {
+      const contentDisplay = summaryContent[key as keyof UserDetailsSummary];
+      const icon = contentDisplay.icon;
+      const title = contentDisplay.title;
+      const data = userListSummary[key as keyof UserDetailsSummary];
+
+      return <CardSimple icon={icon} title={title} data={String(data)} />;
+    });
+  }
+
   return (
     <div>
       <h1>Users</h1>
-      <section>
-        <p>Users: {userListSummary.totalUsers}</p>
-        <p>Active Users: {userListSummary.activeUsers}</p>
-        <p>Users with loan: {userListSummary.loanUsers}</p>
-        <p>Users with savings: {userListSummary.savingsUsers}</p>
-      </section>
+      <section className="user-summary">{getCards()}</section>
       <ul className="userList">
         {userList.map((user) => (
           <li key={user.id}>
