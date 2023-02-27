07188d8e6f9abbb342a81067e503362da9c6ac02
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-02-28 00:57:37 +0100

    feat: Define general rendering of each user detail

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type InfoTypes =
+  | "email"
+  | "tel"
+  | "twitter"
+  | "facebook"
+  | "instagram"
+  | undefined;
+
+type UserInfoProps = {
+  title: string;
+  value: React.ReactNode;
+  type?: InfoTypes;
+};
+export default function UserInfo({ title, value, type }: UserInfoProps) {
+  return (
+    <div>
+      <p className="user-info-title">{title}</p>
+      <p className="user-info-value">{typeHandler(type, value)}</p>
+    </div>
+  );
+}
+
+function typeHandler(type: InfoTypes, value: React.ReactNode) {
+  switch (type) {
+    case "email":
+      return <a href={`mailto:${value}`}>{value}</a>;
+    case "tel":
+      return <a href={`tel:${value}`}>{value}</a>;
+    case "instagram":
+      return <a href={`https://www.instagram.com/${value}`}>{value}</a>;
+    case "facebook":
+      return <a href={`https://facebook.com/${value}`}>{value}</a>;
+    case "twitter":
+      return <a href={`https://twitter.com/${value}`}>{value}</a>;
+    default:
+      return value;
+  }
+}
