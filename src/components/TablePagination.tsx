430bda6a5cc21d19d924700501df4fc6ef8918d5
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-03-05 00:28:54 +0100

    feat: Style table pagination

--- src/components/TablePagination.tsx
+++ src/components/TablePagination.tsx
@@ -1,5 +1,9 @@
 import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
 import { useAppContext } from "../context/context";
+import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+import "./TablePagination.scss";
 
 export default function TablePagination() {
   const {
@@ -12,6 +16,15 @@ export default function TablePagination() {
       count={totalPageCount}
       onChange={handleChangePage}
       shape="rounded"
+      renderItem={(item) => (
+        <PaginationItem
+          slots={{
+            previous: ArrowBackIosRoundedIcon,
+            next: ArrowForwardIosRoundedIcon,
+          }}
+          {...item}
+        />
+      )}
     />
   );
 }
