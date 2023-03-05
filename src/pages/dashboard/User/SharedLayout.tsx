bb5e7e8badc67aa7ce56f3364072a4456f92d327
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-03-05 19:21:22 +0100

    feat: Init styling of user details shared layout

--- src/pages/dashboard/User/SharedLayout.tsx
+++ src/pages/dashboard/User/SharedLayout.tsx
@@ -12,6 +12,8 @@ import Button from "@mui/material/Button";
 import backIcon from "../../../assets/icons/back.svg";
 import "./SharedLayout.scss";
 import Stars from "../../../components/Stars";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
 
 const userRoutes = [
   "/users/:userId/general-details",
@@ -42,11 +44,17 @@ export default function User() {
   const currentTabRoutePattern =
     routeMatch?.pattern.path || "/users/:userId/general-details";
   console.log(currentTabRoutePattern);
+
   const { userList, activateUser, blacklistUser } = useAppContext();
   const user = userList.find((person) => person.id === userId);
   return (
-    <div>
-      <Button component={Link} to=".." startIcon={<img src={backIcon} />}>
+    <div className="user-details-shared">
+      <Button
+        component={Link}
+        to=".."
+        startIcon={<img src={backIcon} />}
+        className="back-button"
+      >
         Back to users
       </Button>
       <section className="header-nav">
@@ -54,6 +62,8 @@ export default function User() {
         <div>
           <Button
             variant="outlined"
+            color="warning"
+            className="blacklist-button"
             disabled={user?.status === "blacklisted"}
             onClick={() => blacklistUser(userId as string)}
           >
@@ -61,7 +71,7 @@ export default function User() {
           </Button>
           <Button
             variant="outlined"
-            color="warning"
+            className="activate-button"
             disabled={user?.status === "active"}
             onClick={() => activateUser(userId as string)}
           >
@@ -71,60 +81,79 @@ export default function User() {
       </section>
       <header>
         <section className="user-summary">
-          <div>
-            <h2>{`${user?.profile.firstName} ${user?.profile.lastName}`}</h2>
-            <p>{user?.accountNumber}</p>
+          <div className="profile-details">
+            <Avatar
+              alt={`${user?.profile.firstName} ${user?.profile.lastName}`}
+              src={user?.profile.avatar}
+              className="avatar"
+            />
+            <div className="profile-info">
+              <h2>{`${user?.profile.firstName} ${user?.profile.lastName}`}</h2>
+              <p>{user?.accountNumber}</p>
+            </div>
           </div>
+          <Divider
+            orientation="vertical"
+            variant="middle"
+            flexItem
+            className="divider"
+          />
           <div className="tier">
             <p>User's Tier</p>
-            <Stars starCount={3} starMax={3} />
+            <Stars starCount={2} starMax={3} />
           </div>
-          <div>
-            <p>₦{user?.accountBalance}</p>
-            <p>
+          <Divider
+            orientation="vertical"
+            variant="middle"
+            flexItem
+            className="divider"
+          />
+          <div className="bank-details">
+            <p className="account-balance">₦{user?.accountBalance}</p>
+            <p className="bank">
               {user?.profile.bvn}/{user?.profile.address} Bank
             </p>
           </div>
         </section>
+        <Tabs value={currentTabRoutePattern} className="tabs">
+          <Tab
+            component={Link}
+            to="general-details"
+            value="/users/:userId/general-details"
+            label="General Details"
+          />
+          <Tab
+            component={Link}
+            to="documents"
+            value="/users/:userId/documents"
+            label="Documents"
+          />
+          <Tab
+            component={Link}
+            to="bank-details"
+            value="/users/:userId/bank-details"
+            label="Bank Details"
+          />
+          <Tab
+            component={Link}
+            to="loans"
+            value="/users/:userId/loans"
+            label="Loans"
+          />
+          <Tab
+            component={Link}
+            to="savings"
+            value="/users/:userId/savings"
+            label="Savings"
+          />
+          <Tab
+            component={Link}
+            to={`/users/${userId}/app-and-system`}
+            value="/users/:userId/app-and-system"
+            label="App and System"
+          />
+        </Tabs>
       </header>
-      <Tabs value={currentTabRoutePattern}>
-        <Tab
-          component={Link}
-          to="general-details"
-          value="/users/:userId/general-details"
-          label="General Details"
-        />
-        <Tab
-          component={Link}
-          to="documents"
-          value="/users/:userId/documents"
-          label="Documents"
-        />
-        <Tab
-          component={Link}
-          to="bank-details"
-          value="/users/:userId/bank-details"
-          label="Bank Details"
-        />
-        <Tab
-          component={Link}
-          to="loans"
-          value="/users/:userId/loans"
-          label="Loans"
-        />
-        <Tab
-          component={Link}
-          to="savings"
-          value="/users/:userId/savings"
-          label="Savings"
-        />
-        <Tab
-          component={Link}
-          to={`/users/${userId}/app-and-system`}
-          value="/users/:userId/app-and-system"
-          label="App and System"
-        />
-      </Tabs>
       <div className="user-details-container">
         <Outlet />
       </div>
