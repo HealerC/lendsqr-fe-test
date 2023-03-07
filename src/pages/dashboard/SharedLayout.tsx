c4ea462d2b06bcb1922e673f1c0be432ca1a3c58
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-03-07 11:18:52 +0100

    docs: Comment code, remove uneccessary lines

--- src/pages/dashboard/SharedLayout.tsx
+++ src/pages/dashboard/SharedLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./SharedLayout.scss";
 import MenuBar from "../../components/MenuBar";
 import AppDrawer from "../../components/AppDrawer";
+
 export default function SharedLayout() {
   return (
     <div className="app-dashboard">
--- src/pages/dashboard/UserList.tsx
+++ src/pages/dashboard/UserList.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 import { useAppContext } from "../../context/context";
 import AppTable from "../../components/AppTable";
-import "./UserList.scss";
-import {
-  useFetch,
-  mapUserDetailsApiToState,
-  summaryContent,
-} from "../../utils";
-import { Button } from "@mui/material";
+import { useFetch, mapUserDetailsApiToState } from "../../utils";
+import Button from "@mui/material/Button";
 import CardsGroup from "../../components/CardsGroup";
+import "./UserList.scss";
 
 const apiUrl =
   "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users";
-const localUrl = "http://localhost:5000/users";
 
+/*
+ * This component represents the route navigated to after successful log in.
+ * It fetches data online from the `apiUrl` above and then transforms it to the
+ * state used in the app using useFetch hook and mapUserDetailsApiToState function.
+ * It then calls the function in context that saves the data to state.
+ */
 export default function UserList() {
   const { setUsers, setLoading } = useAppContext();
   const { data, loading, error } = useFetch(apiUrl, mapUserDetailsApiToState);
@@ -27,6 +28,8 @@ export default function UserList() {
   }, [loading]);
 
   if (error) {
+    // If there was an error fetching the file... Handle the error
+    // by rendering a different component with a button to reload the page
     return (
       <div className="user-list error">
         <h1>Oops! There seems to be a Network error... 😔</h1>
